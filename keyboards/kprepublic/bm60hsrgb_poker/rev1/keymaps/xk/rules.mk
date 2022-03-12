dee5dd28d9dbebe4b52428d7bc35c66939ff8656
Konstantin Koslowski <konstantin.koslowski@posteo.de>
2022-03-12 10:46:15 +0100

    kprepublic/bm60hsrgb_poker:xk enable link time optimization to reduze fw size

--- keyboards/kprepublic/bm60hsrgb_poker/rev1/keymaps/xk/rules.mk
+++ keyboards/kprepublic/bm60hsrgb_poker/rev1/keymaps/xk/rules.mk
@@ -1,4 +1,4 @@
 NKRO_ENABLE = yes             # USB Nkey Rollover
 VIA_ENABLE = yes              # Enable VIA
 SLEEP_LED_ENABLE = yes
-LTO_ENABLE = no               # Use Link Time Optimizations
+LTO_ENABLE = yes               # Use Link Time Optimizations
